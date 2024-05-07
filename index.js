ec16fe604f4eb886883e55f96ebeeb4f45fef647
mianazeemdaula <mianazeemdaula@gmail.com>
2024-05-07 17:33:16 +0500

    btoken

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ require('dotenv').config();
 const { TwitterApi } = require('twitter-api-v2');
 const app = require('express')();
 
-const xClinet = new TwitterApi({
-    appKey: process.env.API_KEY,
-    appSecret: process.env.API_SECRET,
-    accessToken: process.env.ACCESS_TOKEN,
-    accessSecret: process.env.ACCESS_SECRET,
-});
+const xClinet = new TwitterApi(process.env.BEARER_TOKEN);
 async function postTweet() {
     try {
         const d = await xClinet.v2.tweet("Hi dear, I am here to post about programming");

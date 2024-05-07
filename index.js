03a846fba2624f291419f1f6401df0a5a89a65f5
mianazeemdaula <mianazeemdaula@gmail.com>
2024-05-07 16:47:58 +0500

    as

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { TwitterApi } = require('twitter-api-v2');
+const app = require('express')();
 
 const xClinet = new TwitterApi({
     appKey: process.env.API_KEY,
@@ -15,4 +16,20 @@ async function postTweet() {
         console.error(error);
     }
 }
-postTweet();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/tweet', async (req, res) => {
+    try {
+        const d = await xClinet.v2.tweet("Hi dear, I am here to post about programming");
+        res.send(d);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
